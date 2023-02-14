b6900cf451b020feb91e4957d950f5d8cd6b0e4a
YirsonAA <yirsonacevedo@gmail.com>
2023-02-13 19:26:45 -0600

    Commit Inicial

---
+++ variables.php
@@ -0,0 +1,12 @@
+hola
+<?php
+echo 2+2;
+echo"<br>";
+echo 5*5;
+$variableEntera=3;
+$float=3.5;
+$resultado=$variableEntera + $float;
+echo"<br>";
+echo"Muestra el Resultado".$resultado;
+$variableBolena=true;
+echo $variableBolena;
